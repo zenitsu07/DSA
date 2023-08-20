1bc6f88c1efbd32dfbb958b1593c5e560232cf4b
Utkarsh Srivastava <utkarsh.dev83@gmail.com>
2023-08-20 23:02:42 +0530

    Time: 114 ms (92.82%), Space: 57.1 MB (50.80%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
-    
+    //recusrion
+//     int f(int i, vector<int>& nums, int k, vector<vector<int> > &dp) {
+//         if (k == 0)
+//             return 0;
+//         if (i>=nums.size()) return INT_MAX;
+//         if (dp[i][k] != -1) return dp[i][k];
+
+//         int take = max(nums[i], f(i+2, nums, k-1, dp));
+//         int not_take = f(i+1, nums, k, dp);
+//         return dp[i][k] = min(take, not_take);
+//     }
+//     int minCapability(vector<int>& nums, int k) {
+//         vector<vector<int> > dp(nums.size(), vector<int>(k+1, -1));
+//         return f(0, nums, k, dp);
+//     }
     //Binary earch intuition 
     // we start by having max element of array and we will ave mid of that and one by one we will use fucntion isPossible which will iiterate the array to look for k elements lower than equal to mid if those =k => mid is possiblke then we will look for range left to mid -1 else we will search shorter left range i.e. mid+1
     
@@ -38,4 +52,6 @@ public:
         return right;
         
     }
+    
+    
 };
