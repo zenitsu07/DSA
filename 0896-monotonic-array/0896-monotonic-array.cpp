cdb7c0d11e1e9866ee3095a954d5d0ae17cab932
Utkarsh Srivastava <utkarsh.dev83@gmail.com>
2023-09-29 23:30:03 +0530

    Time: 136 ms (15.96%), Space: 96.8 MB (13.85%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
+    // bool isMonotonic(vector<int>& nums) {
+//         bool inc = true, dec = true;
+//         int n = nums.size();
+//         if(n==2)return true;
+//         for(int i =1; i<nums.size();i++){
+            
+//            if(nums[i-1] <= nums[i])inc = true;
+//            else{
+//                inc = false;
+//                break;
+//            }
+                
+//         }
+//         for(int i =1; i<nums.size();i++){
+            
+//            if(nums[i-1] >= nums[i])dec = true;
+//            else{
+//                dec = false;
+//                break;
+//            }
+                
+//         }
+        
+//         return inc || dec;
+//   }
+    
     bool isMonotonic(vector<int>& nums) {
-        bool inc = true, dec = true;
+        
         int n = nums.size();
-        if(n==2)return true;
-        for(int i =1; i<nums.size();i++){
+        bool isIncreasing = false, isDecreasing = false;
+        
+        for(int i = 0; i < n-1; i++) {
             
-           if(nums[i-1] <= nums[i])inc = true;
-           else{
-               inc = false;
-               break;
-           }
-                
-        }
-        for(int i =1; i<nums.size();i++){
+            if(nums[i] < nums[i+1])
+                isIncreasing = true;
+            else if(nums[i] > nums[i+1])
+                isDecreasing = true;
+            if(isIncreasing & isDecreasing) return false;
             
-           if(nums[i-1] >= nums[i])dec = true;
-           else{
-               dec = false;
-               break;
-           }
-                
         }
         
-        return inc || dec;
+        return true;
     }
+    
 };
